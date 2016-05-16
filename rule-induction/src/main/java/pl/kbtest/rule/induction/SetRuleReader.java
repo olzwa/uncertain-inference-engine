4158456ba9499e5dd6366b38a95a32f7845b80e1
tomaszkot1 <kottomasz123@gmail.com>
2016-05-16 15:55:27 +0200

    Fixed SetRuleReaderTest

--- rule-induction/src/main/java/pl/kbtest/rule/induction/SetRuleReader.java
+++ rule-induction/src/main/java/pl/kbtest/rule/induction/SetRuleReader.java
@@ -4,6 +4,8 @@ import pl.kbtest.action.DefaultSetAction;
 import pl.kbtest.action.SetAction;
 import pl.kbtest.contract.SetPremise;
 import pl.kbtest.contract.SetRule;
+import pl.kbtest.rule.induction.exception.InputParsingException;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -15,6 +17,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -26,16 +29,25 @@ public class SetRuleReader {
     Set<String> delimitersSet = null;
     String columnDelimiter = null;
     String conjunctionToken = null;
-    String valueSeparator = ","; //same as pl.kbtest.contract.Config.GLOBAL_SPLIT_REGEX
+    String disjunctionToken = null;
+    //String valueSeparator = ","; //same as pl.kbtest.contract.Config.GLOBAL_SPLIT_REGEX
     boolean transliteration = false;
 
 
 
 
-    SetRuleReader(File file, Set delimiters, String conjunctionToken, boolean transliteration) {
+   /* SetRuleReader(File file, Set delimiters, String conjunctionToken, boolean transliteration) {
         this.file = file;
         this.delimitersSet = delimiters;
         this.conjunctionToken = conjunctionToken;
+        this.transliteration = transliteration;
+    }*/
+
+    public SetRuleReader(File file, Set<String> delimitersSet, String conjunctionToken, String disjunctionToken, boolean transliteration) {
+        this.file = file;
+        this.delimitersSet = delimitersSet;
+        this.conjunctionToken = conjunctionToken;
+        this.disjunctionToken = disjunctionToken;
         this.transliteration = transliteration;
     }
 
@@ -50,48 +62,56 @@ public class SetRuleReader {
             BufferedReader bf = new BufferedReader(new FileReader(file));
             String currentLine = bf.readLine();
 
-            if(transliteration){
-                currentLine = Normalizer.normalize(currentLine,Normalizer.Form.NFD);
-                currentLine=currentLine.replaceAll("'|̨|̇|́","");
-                currentLine=currentLine.replaceAll("ł","l");
-                currentLine=currentLine.replaceAll("Ł","L");
+            if (transliteration) {
+                currentLine = Normalizer.normalize(currentLine, Normalizer.Form.NFD);
+                currentLine = currentLine.replaceAll("'|̨|̇|́", "");
+                currentLine = currentLine.replaceAll("ł", "l");
+                currentLine = currentLine.replaceAll("Ł", "L");
             }
 
             ArrayList<String> delimitersList = new ArrayList<>(delimitersSet);
             ArrayList<Pattern> patterns = new ArrayList<>();
 
             for (String delimiter : delimitersList) {
-                patterns.add(Pattern.compile("\\w"+delimiter+"\\w")); //avoid matching "=" with "=>"
+                patterns.add(Pattern.compile("." + delimiter + "[^>]")); //avoid matching "=" with "=>"
             }
 
 
-
-            for (int i =0;i<patterns.size();i++) {
-                if(patterns.get(i).matcher(currentLine).find()){
+            for (int i = 0; i < patterns.size(); i++) {
+                if (patterns.get(i).matcher(currentLine).find()) {
                     columnDelimiter = delimitersList.get(i);
                     break;
                 }
             }
 
-            if(columnDelimiter == null){throw new IllegalArgumentException("Invalid column delimiters");}
+            if (columnDelimiter == null) {
+                throw new IllegalArgumentException("Invalid column delimiters");
+            }
 
             while (currentLine != null) {
 
-                //currentLine=currentLine.replaceAll("\\[.*\\]","");
-
+                currentLine = currentLine.replaceAll("\\[.*\\]", "");
+                currentLine = currentLine.trim();
                 String[] sides = currentLine.split("=>");
-                if(sides.length!=2){throw new IllegalArgumentException("More than one \"=>\" token in input line");}
+                if (sides.length != 2) {
+                    throw new IllegalArgumentException("More than one \"=>\" token in input line");
+                }
                 String[] leftside = sides[0].split(" ");
 
                 ArrayList<SetPremise> premises = parseLeftSide(leftside);
 
                 String[] rightside = sides[1].split(columnDelimiter);
                 //boolean conjunction = sides[1].contains("AND");
-                boolean conjunction = sides[1].contains(conjunctionToken);
+                //!
+                boolean conjunction = true;
+                //if (disjunctionToken == null){ disjunctionToken = "OR";}
+                conjunction = !sides[1].contains(disjunctionToken);
+
+
                 for (int i = 0; i < rightside.length; i++) {
-                    rightside[i]=rightside[i].trim();
+                    rightside[i] = rightside[i].trim();
                 }
-                SetAction sa = new DefaultSetAction(rightside[0], rightside[1],conjunction );
+                SetAction sa = new DefaultSetAction(rightside[0], rightside[1], conjunction);
                 List actions = new LinkedList<>(Arrays.asList(sa));
 
                 SetRule sr = new SetRule(premises, actions, null);
@@ -103,19 +123,20 @@ public class SetRuleReader {
             bf.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
 
         return rules;
     }
 
-    private ArrayList<SetPremise> parseLeftSide(String[] leftside) {
+    /*private ArrayList<SetPremise> parseLeftSide(String[] leftside) {
 
         StringBuilder sb = new StringBuilder();
         boolean hasHead = false;
         boolean containsDelimiter;
+
+        String nextColumn = "[0-9]/.";
 
         ArrayList<SetPremise> premises = new ArrayList<>();
 
@@ -123,20 +144,29 @@ public class SetRuleReader {
             containsDelimiter = leftside[i].contains(columnDelimiter);
 
             if(hasHead == true && !containsDelimiter){
+
                 sb.append(leftside[i]);
-                if(i == leftside.length-1){
+
+                if(leftside[i].contains(nextColumn)){
 
                     premises.add(getPremise(sb));
                     sb.setLength(0);
+
                 }
             }
             else if(hasHead == true && containsDelimiter){
 
-                premises.add(getPremise(sb));
-                sb.setLength(0);
+                //String[] end = leftside[i].split(columnDelimiter);
+                //if(end.length != 2)
+                //{throw new IllegalArgumentException("More than one column delimiter unseparated by whitespace");}
+               // sb.append(end[0]);
+                //sb.append(end[1])
                 sb.append(leftside[i]);
+                //premises.add(getPremise(sb));
+                sb.setLength(0);
 
-                if(i == leftside.length-1){
+
+                if(leftside[i].contains(nextColumn)){
                     premises.add(getPremise(sb));
                     sb.setLength(0);
                 }
@@ -145,32 +175,79 @@ public class SetRuleReader {
             else if(hasHead == false && containsDelimiter){
                 sb.append(leftside[i]);
                 hasHead=true;
-                if(i == leftside.length-1){
+                if(leftside[i].contains(nextColumn)){
 
                     premises.add(getPremise(sb));
                     sb.setLength(0);
                 }
             }
+            else{
+                sb.append(leftside[i]);
+                hasHead=true;
+            }
+            sb.append(" ");
+        }
+        return premises;
+    }*/
+
+    private ArrayList<SetPremise> parseLeftSide(String[] leftside) {
+
+        StringBuilder sb = new StringBuilder();
+        boolean hasHead = false;
+        boolean containsDelimiter;
+
+        String nextColumn = "[0-9]\\.$";
+        Pattern p = Pattern.compile(nextColumn);
+
+
+        ArrayList<SetPremise> premises = new ArrayList<>();
+
+        for (int i = 0; i < leftside.length; i++) {
+            Matcher m = p.matcher(leftside[i]);
+
+            if (m.find()) {
+                if (hasHead) {
+                    premises.add(getPremise(sb));
+                    hasHead = false;
+                    sb.setLength(0);
+                    sb.append(leftside[i]);
+                } else {
+                    sb.append(leftside[i]);
+                    hasHead = true;
+                }
+            }
+            else {
+                sb.append(leftside[i]);
+                hasHead = true;
+            }
+
+            if (i == leftside.length - 1) {
+                premises.add(getPremise(sb));
+            }
+            sb.append(" ");
 
         }
+
         return premises;
     }
 
-    private SetPremise getPremise(StringBuilder sb){
+    private SetPremise getPremise(StringBuilder sb) {
         String temp = sb.toString();
-        boolean conjunction=false;
-        if(temp.contains(conjunctionToken)){
-            conjunction = true;
-            temp=temp.replaceAll(conjunctionToken,",");
+        boolean conjunction = true;
+        if (temp.contains(disjunctionToken)) {
+            conjunction = false;
         }
 
-        String regex = valueSeparator+"|"+columnDelimiter+"|"+conjunctionToken;
+        String regex = columnDelimiter + "|" + conjunctionToken;
         String[] parts = temp.split(regex);
+        for (int i = 0; i < parts.length; i++) {
+            parts[i]=parts[i].trim();
+        }
         Set premiseSet = new HashSet<>();
-        for (int i = 1; i <parts.length ; i++) {
+        for (int i = 1; i < parts.length; i++) {
             premiseSet.add(parts[i]);
         }
-        SetPremise premise = new SetPremise(parts[0],premiseSet,false,conjunction);
+        SetPremise premise = new SetPremise(parts[0], premiseSet, false, conjunction);
         return premise;
     }
 
--- rule-induction/src/test/java/pl/kbtest/rule/induction/SetRuleReaderTest.java
+++ rule-induction/src/test/java/pl/kbtest/rule/induction/SetRuleReaderTest.java
@@ -5,6 +5,8 @@ import com.thoughtworks.xstream.XStream;
 import org.custommonkey.xmlunit.DetailedDiff;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.Difference;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 import pl.kbtest.action.DefaultSetAction;
@@ -19,6 +21,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.Assert.*;
+import org.javers.*;
 
 /**
  * Created by Ja on 2016-04-05.
@@ -27,43 +30,69 @@ public class SetRuleReaderTest {
 
     @Test
     public void testReadRules() throws Exception {
-        Set delimiters = new HashSet<>(Arrays.asList("=", ":"));
-        String conjunctionToken = new String("AND");
+        Set delimiters = new HashSet<>(Arrays.asList("="));
+        String conjunctionToken = "AND";
+        String disjunctionToken = "OR";
         File f = new File(SetRuleReader.class.getClassLoader().getResource("SetRuleReaderTestFile.txt").getFile());
 
-        SetPremise first = new SetPremise("wiek", new HashSet<>(Arrays.asList("kolejna_wartosc", "21")), false, false);
-        SetPremise second = new SetPremise("plec", new HashSet<>(Arrays.asList("kobieta")), false, false);
-        SetPremise third = new SetPremise("stan_zdrowia", new HashSet<>(Arrays.asList("kolejna_wartosc2", "dobry")), false, false);
-        //SetAction action = new DefaultSetAction("motywacja", "przyjemność płynąca z uprawiania sportu2 AND troska o wygląd2", true);
-        SetAction action = new DefaultSetAction("motywacja", "przyjemnosc plynaca z uprawiania sportu2 AND troska o wyglad2", true);
+        // [3,3] 16. Scharakteryzuj stan swojego zdrowia.=dobry 8. Scharakteryzuj najczęściej używaną przez Ciebie odzież sportową. =markowa
+        // AND średniej klasy 1. Podaj swoją płeć. Zaznacz odpowiedź. =kobieta 2. Podaj swój wiek (w latach):=21
+        // => 9. Na co przeznaczasz zasadniczą część swoich wakacji (urlopu)? =spotkania towarzyskie AND sen i lektura
+
+        SetPremise first = new SetPremise("16. Scharakteryzuj stan swojego zdrowia.", new HashSet<>(Arrays.asList("dobry")), false, true);
+        SetPremise second = new SetPremise("8. Scharakteryzuj najczęściej używaną przez Ciebie odzież sportową.", new HashSet<>(Arrays.asList("markowa","średniej klasy")), false, true);
+        SetPremise third = new SetPremise("1. Podaj swoją płeć. Zaznacz odpowiedź.", new HashSet<>(Arrays.asList("kobieta")), false, true);
+        SetPremise fourth = new SetPremise("2. Podaj swój wiek (w latach):", new HashSet<>(Arrays.asList("21")), false, true);
+        SetAction action = new DefaultSetAction("9. Na co przeznaczasz zasadniczą część swoich wakacji (urlopu)?", "spotkania towarzyskie AND sen i lektura", true);
         List<SetPremise> premises = new ArrayList();
         premises.add(first);
         premises.add(second);
         premises.add(third);
-       // List conclusions = Collections.singletonList(action);
+        premises.add(fourth);
         List conclusions = Arrays.asList(action);
-
-
         SetRule rule = new SetRule(premises, conclusions, null);
         List<SetRule> expectedRules = new ArrayList<>();
         expectedRules.add(rule);
 
-        first = new SetPremise("kolumna", new HashSet<>(Arrays.asList("wartosc2", "wartosc1")), false, true);
-        second = new SetPremise("kolumna2", new HashSet<>(Arrays.asList("wartosc4", "wartosc3")), false, false);
-        third = new SetPremise("kolumna3", new HashSet<>(Arrays.asList("wartosc5", "wartosc6")), false, false);
-        action = new DefaultSetAction("fakt", "wartosc7", false);
+        //[3,3] 13. Oceń swoje dotychczasowe postępy w nauce i pracy. =bardzo dobre
+        // 16. Scharakteryzuj stan swojego zdrowia.=dobry 1. Podaj swoją płeć. Zaznacz odpowiedź. =kobieta
+        // 2. Podaj swój wiek (w latach):=21  => 11. Czy palisz papierosy? Zaznacz odpowiedź. =sporadycznie
+
+        first = new SetPremise("13. Oceń swoje dotychczasowe postępy w nauce i pracy.", new HashSet<>(Arrays.asList("bardzo dobre")), false, true);
+        second = new SetPremise("16. Scharakteryzuj stan swojego zdrowia.", new HashSet<>(Arrays.asList("dobry")), false, true);
+        third = new SetPremise("1. Podaj swoją płeć. Zaznacz odpowiedź.", new HashSet<>(Arrays.asList("kobieta")), false, true);
+        fourth = new SetPremise("2. Podaj swój wiek (w latach):", new HashSet<>(Arrays.asList("21")), false, true);
+        action = new DefaultSetAction("11. Czy palisz papierosy? Zaznacz odpowiedź.", "sporadycznie", true);
         premises = new ArrayList<>();
         premises.add(first);
         premises.add(second);
         premises.add(third);
-
-        conclusions =Arrays.asList(action); //Collections.singletonList(action);
-
-
+        premises.add(fourth);
+        conclusions =Arrays.asList(action);
         rule = new SetRule(premises, conclusions, null);
         expectedRules.add(rule);
 
-        SetRuleReader srr = new SetRuleReader(f, delimiters, conjunctionToken,true);
+        //13. Oceń swoje dotychczasowe postępy w nauce i pracy. =bardzo dobre
+        // 16. Scharakteryzuj stan swojego zdrowia.=dobry 1. Podaj swoją płeć. Zaznacz odpowiedź. =kobieta
+        // 2. Podaj swój wiek (w latach):=21  => 3. Do jakiej kategorii (I) należą uprawiane przez Ciebie sporty?=sporty całoroczne OR sporty zimowe
+
+        first = new SetPremise("13. Oceń swoje dotychczasowe postępy w nauce i pracy.", new HashSet<>(Arrays.asList("bardzo dobre")), false, true);
+        second = new SetPremise("16. Scharakteryzuj stan swojego zdrowia.", new HashSet<>(Arrays.asList("dobry")), false, true);
+        third = new SetPremise("1. Podaj swoją płeć. Zaznacz odpowiedź.", new HashSet<>(Arrays.asList("kobieta")), false, true);
+        fourth = new SetPremise("2. Podaj swój wiek (w latach):", new HashSet<>(Arrays.asList("21")), false, true);
+        action = new DefaultSetAction("3. Do jakiej kategorii (I) należą uprawiane przez Ciebie sporty?", "sporty całoroczne OR sporty zimowe", false);
+        premises = new ArrayList<>();
+        premises.add(first);
+        premises.add(second);
+        premises.add(third);
+        premises.add(fourth);
+        conclusions =Arrays.asList(action);
+        rule = new SetRule(premises, conclusions, null);
+        expectedRules.add(rule);
+
+
+//
+        SetRuleReader srr = new SetRuleReader(f, delimiters, conjunctionToken, disjunctionToken,false);
 
         List<SetRule> result = srr.readRules();
 
@@ -73,7 +102,7 @@ public class SetRuleReaderTest {
         if(eq){
             System.out.println("deep equals returns true");
         }
-        XStream xs = new XStream();
+      /*  XStream xs = new XStream();
 
         String xmlResult = xs.toXML(result);
         String xmlExpected = xs.toXML(expectedRules);
@@ -92,8 +121,21 @@ public class SetRuleReaderTest {
             System.out.println(dtemp);
             System.out.println("///////");
         }
+*/
+        //javers
+        Javers javers = JaversBuilder.javers().build();
+        org.javers.core.diff.Diff diff = javers.compareCollections(result,expectedRules,SetRule.class);
+        System.out.println("Javers diff: \n"+diff);
+        //
 
+        /*for (int i = 0; i < result.size() ; i++) {
+            System.out.println("compared rules: "+i);
+            Assert.assertEquals(result.get(i),expectedRules.get(i));
+        }*/
+        Assert.assertEquals(diff.getChanges().size(),0);
         //Assert.assertEquals(result,expectedRules);
        //assertEquals(result,expectedRules);
+
+
     }
 }
