4e4356243699ccda7265c88de2236c194a06c0ff
tomaszkot1 <t.kot@poczta.fm>
2016-10-30 12:18:47 +0100

    Serializing and deserializing SetRule to JSON witth GSON

---
+++ contract/src/main/java/pl/kbtest/action/SetActionAdapter.java
@@ -0,0 +1,42 @@
+package pl.kbtest.action;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Member;
+import java.lang.reflect.Type;
+
+/**
+ * Created by tomasz on 30.10.16.
+ */
+public class SetActionAdapter implements JsonSerializer<SetAction>, JsonDeserializer<SetAction> {
+
+    @Override
+    public JsonElement serialize(SetAction src, Type sa, JsonSerializationContext context) {
+        return context.serialize(src);
+    }
+
+    @Override
+    public SetAction deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+
+        String fact = null;
+        try {
+            fact = jsonElement.getAsJsonObject().get("fact").getAsString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String output = null;
+        try {
+            output = jsonElement.getAsJsonObject().get("output").getAsString();
+        } catch (UnsupportedOperationException e) {
+            //e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Boolean conj = new Boolean(jsonElement.getAsJsonObject().get("conjunction").getAsString());
+
+        return new DefaultSetAction(fact, output, conj);
+
+    }
+}
---
+++ rule-induction/src/test/java/pl/kbtest/rule/induction/GSONSetRuleTest.java
@@ -0,0 +1,59 @@
+package pl.kbtest.rule.induction;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.junit.Assert;
+import pl.kbtest.action.SetActionAdapter;
+import pl.kbtest.action.DefaultSetAction;
+import pl.kbtest.action.SetAction;
+import pl.kbtest.contract.GrfIrf;
+import pl.kbtest.contract.SetPremise;
+import pl.kbtest.contract.SetRule;
+;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Created by tomasz on 27.10.16.
+ */
+public class GSONSetRuleTest {
+
+    public static void main(String[] args) {
+
+        SetPremise first = new SetPremise("wydzial_rodzimy", new HashSet<>(Arrays.asList("informatyka", "elektryk")), false, true);
+        SetAction action = new DefaultSetAction("kierunek=informatyka", null, true);
+        List<SetPremise> premises = new ArrayList();
+        premises.add(first);
+        List conclusions = Arrays.asList(action);
+        SetRule rule = new SetRule(premises, conclusions, new GrfIrf(BigDecimal.valueOf(0.55), BigDecimal.valueOf(0.75)));
+
+        System.out.println(rule);
+
+        Gson gs = new GsonBuilder().serializeNulls().disableHtmlEscaping()/*.setPrettyPrinting().*/
+                .registerTypeAdapter(SetAction.class, new SetActionAdapter())
+                .create();
+
+        String json = gs.toJson(rule);
+
+        System.out.println(json);
+
+        SetRule fromJSON = gs.fromJson(json, SetRule.class);
+
+        Javers javers = JaversBuilder.javers().build();
+        org.javers.core.diff.Diff diff = javers.compare(rule, fromJSON);
+        System.out.println(diff);
+
+
+        Assert.assertEquals(rule, fromJSON);
+        //Assert.assertEquals(0, diff.getChanges().size());
+
+
+    }
+
+}
