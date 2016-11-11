6d3471f1aad59da5de3145b85cc073b168b58ca7
tomaszkot1 <kottomasz123@gmail.com>
2016-11-11 17:42:22 +0100

    GsonSetRuleTest reading from file

--- rule-induction/src/test/java/pl/kbtest/rule/induction/GsonSetRuleTest.java
+++ rule-induction/src/test/java/pl/kbtest/rule/induction/GsonSetRuleTest.java
@@ -12,8 +12,8 @@ import pl.kbtest.action.SetAction;
 import pl.kbtest.contract.GrfIrf;
 import pl.kbtest.contract.SetPremise;
 import pl.kbtest.contract.SetRule;
-;
 
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,6 +55,48 @@ public class GsonSetRuleTest {
         Assert.assertEquals(rule, fromJSON);
         //Assert.assertEquals(0, diff.getChanges().size());
 
+
+    }
+
+    @Test
+    public void readFromFile() {
+        Gson gs = new GsonBuilder().serializeNulls().disableHtmlEscaping()/*.setPrettyPrinting().*/
+                .registerTypeAdapter(SetAction.class, new SetActionAdapter())
+                .create();
+        List<SetRule> rules = new ArrayList<>();
+
+        SetPremise first = new SetPremise("wydzial_rodzimy", new HashSet<>(Arrays.asList("informatyka", "elektryk")), false, true);
+        SetAction action = new DefaultSetAction("kierunek=informatyka", null, true);
+        List<SetPremise> premises = new ArrayList();
+        premises.add(first);
+        List conclusions = Arrays.asList(action);
+        SetRule rule = new SetRule(premises, conclusions, new GrfIrf(BigDecimal.valueOf(0.55), BigDecimal.valueOf(0.75)));
+        rules.add(rule);
+
+        List<SetRule> rulesFromFile = new ArrayList<>();
+
+        try {
+
+            File file = new File(SetFactReader.class.getClassLoader().getResource("GsonSetRuleTestFile.txt").getFile());
+            BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+            String currentLine = bf.readLine();
+
+            while (currentLine != null) {
+                SetRule ruleFromFile = gs.fromJson(currentLine,SetRule.class);
+                rulesFromFile.add(ruleFromFile);
+                currentLine = bf.readLine();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Javers javers = JaversBuilder.javers().build();
+        org.javers.core.diff.Diff diff = javers.compareCollections(rules, rulesFromFile, SetRule.class);
+        System.out.println(diff);
+
+        Assert.assertEquals(0, diff.getChanges().size());
 
     }
 
